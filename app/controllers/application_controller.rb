60bb302d45c43ffcc4b4761a8fe6c3f0c300f311
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-02-26 02:05:32 +0000

    [Update] 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
