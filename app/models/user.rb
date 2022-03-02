6e6a70732c82f3bfe418f129fc7595eb1a6f7960
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-02 12:52:15 +0000

    [Add] search

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -39,4 +40,18 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
+  get 'search' => 'searches#search'
+
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
