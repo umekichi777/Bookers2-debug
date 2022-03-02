341df2e8e7d0eb5ef7f8f1fd7ac0de199ad9c5dd
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-02 15:46:37 +0000

    [Finish] search10

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,17 +41,15 @@ class User < ApplicationRecord
   end
 
 
-  def self.looks(search, word)
+  def self.search_for(search, word)
     if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
+      User.where(name: word)
     elsif search == "forward_match"
-      @user = User.where("name LIKE?", "#{word}%")
+      User.where("name LIKE?", word+'%')
     elsif search == "backward_match"
-      @user = User.where("name LIKE?", "%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?", "%#{word}%")
+      User.where("name LIKE?", '%'+word)
     else
-      @user = User.all
+      User.where("name LIKE?", '%'+word+'%')
     end
   end
 end
