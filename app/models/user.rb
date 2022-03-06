29d77d4762868d7880a5658e129ddf3ce8a50ee5
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-06 17:28:49 +0000

    [Update] follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-
+  # 自分がフォローする側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-
+  # 自分がフォローしている人　（一覧画面などで使う　user.followings）
   has_many :followings, through: :relationships, source: :followed
+
+  # 自分がフォローされる側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分をフォローしている人　（一覧画面などで使う　user.followers）
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
@@ -28,14 +31,15 @@ class User < ApplicationRecord
   end
 
 
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
+  # フォローしたときの処理
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
-
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+  # フォローを外したときの処理
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
-
+  # フォローしているかの判定
   def following?(user)
     followings.include?(user)
   end
