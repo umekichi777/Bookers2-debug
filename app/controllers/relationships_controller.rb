48d40acb9917b06af560937c04fc117564f23d6b
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-06 17:42:55 +0000

    [Update] follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,24 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
-  end
-  
-  def destroy
-    current_user.unfollow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.follow(user)
     redirect_to request.referer
   end
 
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+    redirect_to request.referer
+  end
+
+# フォロー　一覧　（フォローのindex）
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
   end
-
+# フォロワー　一覧　（フォロワーのindex）
   def followers
     user = User.find(params[:user_id])
     @users = user.followers
