98c47e3e8715c0033f7ce665146ccfd4ce18bae8
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-08 06:49:05 +0000

    [Update] groupuser

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User'
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users, source: :user
 
