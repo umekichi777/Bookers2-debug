79e4346160d3cf0c60e8f34d1117aa66d1e3b9d2
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-10 07:10:08 +0000

    [Add] chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy
 
   # 自分がフォローする側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
